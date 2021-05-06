da7e82b97178197e9f5e648119a163d6bac379f1
jv6ix9ine <joathvera09@gmail.com>
2021-05-06 12:17:11 -0500

    New Calculator

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function reset(){
+    document.getElementById('reset').reset;
+}
+
+function sumar(){
+    var x = parseInt(document.getElementById('num1').value);
+    var y = parseInt(document.getElementById('num2').value);
+    document.getElementById('resultado').innerHTML = x + y;
+}
+
+function restar(){
+    var x = parseInt(document.getElementById('num1').value);
+    var y = parseInt(document.getElementById('num2').value);
+    document.getElementById('resultado').innerHTML = x - y;
+}
+
+function multiplicar(){
+    var x = parseInt(document.getElementById('num1').value);
+    var y = parseInt(document.getElementById('num2').value);
+    document.getElementById('resultado').innerHTML = x * y;
+}
+
+function dividir(){
+    var x = parseInt(document.getElementById('num1').value);
+    var y = parseInt(document.getElementById('num2').value);
+    document.getElementById('resultado').innerHTML = x / y;
+}
